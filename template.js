809d00e6b1c9784014351b226f853e03d900b633
zakuni <kunio038@gmail.com>
2010-08-06 14:27:36 +0900

    画像を使用したメニューに変更。 コンテンツのメニューを上へデザイン変更。 不要部分を削除し、若干軽くなった、かも。 iPhone最適化もちょっと。

--- template.js
+++ template.js
@@ -1,18 +1,52 @@
+
 function header(title, subtitle){
 
-    document.write("<table border='0' width='500px' cellpadding='0' cellspacing='5'>");
-    document.write("<tr align='center'>	    <td>");
-    document.write("<table bgcolor='#000000' width='100%' cellpadding='10'>		<tr bgcolor='#ffffff' align='center'>		  <td>");
-    document.write("<font color='#ffffff' size='4'>		      " + subtitle + "		    </font>");
-    document.write("</td>		</tr>	      </table>	    </td>");
-    document.write("<td width='40%'>	      <table bgcolor='#000000' width='100%' cellpadding='10'>		<tr bgcolor='#ffffff' align='center'>		  <td>");
-    document.write("<font size='4'>		      <b>" + title + "</b>		    </font>		  </td>		</tr>	      </table>	    </td>");
-    document.write("<td>	      <table bgcolor='#000000' width='100%' cellpadding='10'>		<tr bgcolor='#ffffff' align='center'>		  <td>		    <font color='#ffffff' size='4'>		      " + subtitle + "		    </font>		  </td>");
-    document.write("</tr>	      </table>	    </td>	  </tr>");
-    document.write("<tr>	    <td colspan='3'>	      <table border='0' bgcolor='#000000' width='100%' cellspacing='2' cellpadding='3'>		<tr bgcolor='#ffffff'>		  <td align='center'>");
-    document.write("<font size='3'><a href='./'>トップ</a>　<a href='profile.html'>プロフィール</a>　<a href='photo.html'>写真</a>　<a href='http://jbbs.livedoor.jp/movie/5730/'>掲示板</a>　<a href='words.html'>文書</a>　<a href='something.html'>なんか</a>　<a href='mailto:kunio038&#64;gmail.com'>メール</a></font>");
-    document.write("</td>		</tr>	      </table>");
-    document.write("</td>	  </tr>	</table>");
+
+    document.write("<table class='contents' cellpadding='0' cellspacing='5'>");
+    document.write("<tr>");
+    document.write("<td>");
+    document.write("<table class='side'><tr class='side'><td>");
+    document.write("<p class='side'>" + subtitle + "</p>");
+    document.write("</td></tr></table>");
+    document.write("</td>");
+    document.write("<td width='40%'><table class='title' cellpadding='10'><tr class='title'><td>");
+    document.write("<p class='pagetitle'>" + title + "</p></td></tr></table></td>");
+    document.write("<td><table class='side'><tr class='side'><td><p class='side'>" + subtitle + "</p></td>");
+    document.write("</tr></table></td></tr>");
+    document.write("<tr><td colspan='3'>");
+    document.write("<table border='0' bgcolor='#000000' width='100%' cellspacing='2' cellpadding='3'><tr bgcolor='#ffffff'><td align='center'>");
+    document.write("<a href='./'><img src='Home.png'></a><span class='pc'>　</span><a href='profile.html'><img src='ID_b.png'></a><span class='pc'>　</span><a href='photo.html'><img src='Photos_b.png'></a><span class='pc'>　<a href='http://jbbs.livedoor.jp/movie/5730/'><img src='Talk_b.png'></a></span><span class='pc'>　</span><a href='words.html'><img src='Text_b.png'></a><span class='pc'>　</span><a href='something.html'><img src='Something_b.png'></a><span class='pc'>　</span><a href='mailto:kunio038&#64;gmail.com'><img src='Mail_b.png'></a>");
+    document.write("</td></tr></table>");
+    document.write("</td></tr></table>");
+
+/*
+
+    document.write("<table class='contents' cellpadding='0' cellspacing='5'>");
+    document.write("<tr><td colspan='3'>");
+    document.write("<table border='0' bgcolor='#000000' width='100%' cellspacing='2' cellpadding='3'><tr bgcolor='#ffffff'><td align='center'>");
+    document.write("<a href='./'><img src='Home.png'></a><span class='pc'>　</span><a href='profile.html'><img src='ID_b.png'></a><span class='pc'>　</span><a href='photo.html'><img src='Photos_b.png'></a><span class='pc'>　<a href='http://jbbs.livedoor.jp/movie/5730/'><img src='Talk_b.png'></a></span><span class='pc'>　</span><a href='words.html'><img src='Text_b.png'></a><span class='pc'>　</span><a href='something.html'><img src='Something_b.png'></a><span class='pc'>　</span><a href='mailto:kunio038&#64;gmail.com'><img src='Mail_b.png'></a>");
+    document.write("</td></tr></table>");
+    document.write("</td></tr>");
+    document.write("<tr>");
+    document.write("<td>");
+
+     document.write("<table class='side'><tr class='side'><td>");
+     document.write("<p class='side'>" + subtitle + "</p>");
+     document.write("</td></tr></table>");
+
+    document.write("</td>");
+
+     document.write("<td width='40%'><table class='title' cellpadding='10'><tr class='title'><td>");
+     document.write("<p class='pagetitle'>" + title + "</p></td></tr></table></td>");
+
+    document.write("<td>");
+
+    document.write("<table class='side'><tr class='side'><td><p class='side'>" + subtitle + "</p></td>");
+    document.write("</tr></table>");
+
+    document.write("</td></tr>");
+    document.write("</table>");
+*/
 
 }
 
@@ -21,4 +55,19 @@ function contents(){
 }
 
 
+var colors = new Array("#ebd763", "#64cec7", "#f0886a", "#57c062", "#8e57c0")
+var rcolor = colors[Math.floor(Math.random()*colors.length)]
+
+function changeCSS(){
+    $("table.index td.index").css("background-color", rcolor);
+    $("table.index td.index").css("border-right-color", rcolor);
+}
+
+function since(){
+    if(navigator.userAgent.indexOf('Mobile') > -1){
+    }
+    else{
+        document.write("<p class='since'>since 1.29 2005</p><br>");
+    }
+}
 
